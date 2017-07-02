b93373acc942c9c90f22be4d4680c50f3fe42a2d
Tatsuya Arai <araitatsuyaj@gmail.com>
2017-07-01 19:40:31 -0500

    Add files via upload

--- eric_said2.m
+++ eric_said2.m
@@ -1,12 +1,13 @@
 function eric_said2
 
-keyboard; 
+%keyboard; 
 % It is challenging to avoide O(N^2)...
 % My first try. 
 
+
 %%% Initial Matrix
-m = 12
-n = 12
+m = 4
+n = 6
 
 A = round(rand(m,n));
 
@@ -23,23 +24,27 @@ for i1 = 1:m_max
     for i2 = 1:n_max
         Mask = zeros(m,n);
         Mask([1:i1],[1:i2]) = 1;
-        %if sum(sum(Mask)) > p_rec_max
+
         if sum(sum(Mask)) > p_rec_max || sum(sum(Mask)) > m_max * n_max
             break
         else
-            %imagesc(Mask);
-            %pause();
+
             mask = Mask(:);
             mask_stack_unique = [mask_stack_unique, mask];
             mask_stack = [mask_stack, mask];
             mask_shift = mask;
+            wrap_line = i1;
             for i3 = 1:n*m
                 mask_shift([2:end]) = mask_shift([1:end-1]);
                 mask_shift(1) = 0;
-                mask_stack = [mask_stack, mask_shift];
+                
+                if rem(i3,m) + i1 <= m %&& %rem(i3,m) ~= 0
+                    mask_stack = [mask_stack, mask_shift];
+                end
                 if mask_shift(end) == 1
                     break
                 end
+             
             end
         end
     end
@@ -57,12 +62,20 @@ loc2 = loc1(find(rect_eval2 == max(rect_eval2)))
 Mask_2 = zeros(size(A));
 for i1 = 1:length(loc2)
     mask_2 = mask_stack(:,loc2(i1));
-    Mask_2 = Mask_2 + reshape(mask_2,[m,n]);
+    Mask_2_temp = reshape(mask_2,[m,n]);
+    %if max(sum(Mask_2_temp,2)) * max(sum(Mask_2_temp,1)) == max(rect_eval2)
+    wrap_eval_h = sum(Mask_2_temp,1);
+    wrap_eval_v = sum(Mask_2_temp,2);
+    wrap_eval_h = wrap_eval_h(find(wrap_eval_h ~= 0 ));
+    wrap_eval_v = wrap_eval_v(find(wrap_eval_v ~= 0 ));
+    if std(wrap_eval_h) == 0 &&  std(wrap_eval_v) == 0
+        Mask_2 = Mask_2 + reshape(mask_2,[m,n]);
+    end
 end
 
 figure(2);
-subplot(131); imagesc(A);
-subplot(132); imagesc(Mask_2);
-subplot(133); imagesc(A + Mask_2);
+subplot(131); imagesc(A); axis square;
+subplot(132); imagesc(Mask_2); axis square;
+subplot(133); imagesc(A + Mask_2); axis square;
 
 
