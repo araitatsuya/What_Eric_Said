091b1904f397a71984c0c5098a1ef0ddd34443c2
Tatsuya Arai <araitatsuyaj@gmail.com>
2017-07-08 22:01:51 -0500

    Add files via upload

--- Eric_Said1_2.py
+++ Eric_Said1_2.py
@@ -1,8 +1,14 @@
 import numpy as np
 import numpy.matlib as M
+import numpy.random as random
 
 max_r = 20
-r = np.around(max_r * M.rand(10, 1))
+r = np.around(max_r * random.rand(10, 1))
 
 np.nonzero(np.dot(r,r.T)/r + r == 24)
+loc1 = np.nonzero(np.dot(r,r.T)/r + r == 24)[0]
+loc2 = np.nonzero(np.dot(r,r.T)/r + r == 24)[1]
 
+for i0 in range(len(loc1)):
+    print(loc1[i0], ':', r[int(loc1[i0])], loc2[i0], ':', r[int(loc2[i0])])
+print(r)
